68df3631213f6de5df8c4b46debdadf4373cb1ee
Eric Tsai <eric492718@gmail.com>
2025-02-10 17:58:45 +0800

    create db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,6 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
-import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { index, integer, pgTableCreator, varchar } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -18,19 +10,28 @@ import {
  */
 export const createTable = pgTableCreator((name) => `google-drive_${name}`);
 
-export const posts = createTable(
-  "post",
+export const files = createTable(
+  "files",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar("name", { length: 256 }).notNull(),
+    size: integer("size").notNull(),
+    url: varchar("url", { length: 256 }).notNull(),
+    parent: integer("parent").notNull(),
+  },
+  (t) => ({
+    parentIndex: index("files_parent_idx").on(t.parent),
+  }),
+);
+
+export const folders = createTable(
+  "folders",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
     name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
+    parent: integer("parent").notNull(),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (t) => ({
+    parentIndex: index("folders_parent_idx").on(t.parent),
+  }),
 );
