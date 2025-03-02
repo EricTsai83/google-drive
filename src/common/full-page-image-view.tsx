80a9086dbb7bce0268a6e15b481a12082c602a16
Eric Tsai <eric492718@gmail.com>
2025-03-03 01:45:54 +0800

    create a blur Image component for loading blur effect

--- src/common/full-page-image-view.tsx
+++ src/common/full-page-image-view.tsx
@@ -1,33 +1,16 @@
-"use client";
-
 // import { Button } from "@/components/ui/button";
 import { type files_table } from "@/server/db/schema";
-import Image from "next/image";
-import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { BlurImage } from "@/components/blur-image";
 
 export function FullPageImageView({
   image,
 }: {
   image: typeof files_table.$inferSelect;
 }) {
-  const [isLoading, setLoading] = useState(true);
   return (
     <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
       <div className="flex-shrink flex-grow">
-        <Image
-          src={image.url}
-          width={1000}
-          height={1000}
-          alt=""
-          className={cn(
-            "duration-700 ease-in-out",
-            isLoading
-              ? "scale-105 blur-2xl grayscale"
-              : "scale-100 blur-0 grayscale-0",
-          )}
-          onLoadingComplete={() => setLoading(false)}
-        />
+        <BlurImage src={image.url} width={1000} height={1000} alt="" />
 
         {/* eslint-disable-next-line @next/next/no-img-element */}
         {/* <img src={image.url} className="object-contain" alt={image.name} /> */}
---
+++ src/components/blur-image.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import cn from "clsx";
+
+export function BlurImage(props: React.ComponentProps<typeof Image>) {
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <Image
+      {...props}
+      alt={props.alt}
+      className={cn(
+        props.className,
+        "duration-700 ease-in-out",
+        isLoading
+          ? "scale-110 blur-2xl grayscale"
+          : "scale-100 blur-0 grayscale-0",
+      )}
+      onLoadingComplete={() => setLoading(false)}
+    />
+  );
+}
